d249a7f9cbb59cda8bc47cbecd928b8086403149
Loice-Andia <loiceandia@gmail.com>
2016-08-16 12:18:11 +0300

    Modifies add person to check all args are passed

--- app/person/personClass.py
+++ app/person/personClass.py
@@ -13,6 +13,8 @@ class Person(object):
     	"""Adds a person to the system and allocates the person to a random room."""
     	self.person_name = args["<first_name>"] + " " + args["<last_name>"]
         self.wants_accomodation = args["<wants_accomodation>"]
+        print self.person_name
+        print args["<wants_accomodation>"]
 
 
     def load_people(self):
