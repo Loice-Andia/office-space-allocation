e0415e4ff5a7aaeebae5469c5a8afac441484152
andela-lolo <loice.andia@andela.com>
2016-09-05 17:52:11 +0300

    Intergrate travis badge

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='office-space-allocation',
+    description='Checkpoint 1',
+    author='Loice Andia',
+    author_email='loice.andia@andela.com',
+    url='https://github.com/andela-lolo/office-space-allocation',
+    version='1.0.0'
+)
